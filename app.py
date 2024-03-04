11c890193b3480aff2c0e8e3e4586153d4193ce5
Tuba_Khan <121151121+tubakhan0415@users.noreply.github.com>
2024-03-04 12:27:42 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template, request, jsonify
+from flask_cors import CORS
+
+from chat import get_response
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route("/")  # Correcting the decorator here
+def index_get():
+    return render_template("base.html")
+
+@app.route("/predict", methods=["POST"])  # Added methods parameter to specify POST
+def predict():
+    text = request.get_json().get("message")
+    # TODO: check if text is valid
+    response = get_response(text)
+    message = {"answer": response}
+    return jsonify(message)
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ chat.py
@@ -0,0 +1,69 @@
+import random
+import json
+
+import torch
+
+from model import NeuralNet
+from nltk_utils import bag_of_words, tokenize
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+with open('intents.json', 'r') as json_data:
+    intents = json.load(json_data)
+
+FILE = "data.pth"
+data = torch.load(FILE)
+
+input_size = data["input_size"]
+hidden_size = data["hidden_size"]
+output_size = data["output_size"]
+all_words = data['all_words']
+tags = data['tags']
+model_state = data["model_state"]
+
+model = NeuralNet(input_size, hidden_size, output_size).to(device)
+model.load_state_dict(model_state)
+model.eval()
+
+bot_name = "Sam"
+
+def get_response(msg, default_link="https://f2tech.streamlit.app/"):
+    sentence = tokenize(msg)
+    X = bag_of_words(sentence, all_words)
+    X = X.reshape(1, X.shape[0])
+    X = torch.from_numpy(X).to(device)
+
+    output = model(X)
+    _, predicted = torch.max(output, dim=1)
+
+    tag = tags[predicted.item()]
+
+    probs = torch.softmax(output, dim=1)
+    prob = probs[0][predicted.item()]
+
+    button_style = "padding: 10px; background-color: #000000; color: white; border: none; border-radius: 5px; cursor: pointer;"
+
+    if prob.item() > 0.75:
+        for intent in intents['intents']:
+            if tag == intent["tag"]:
+                if intent["responses"] == ["Thanks! I will inform you in a minute."]:
+                    return f"To Calculate Your Loan <button style=\"{button_style}\" onclick=\"window.open('{default_link}', '_blank')\">Click here</button>"
+                elif 'link' in intent:
+                    return f"For more information, visit: <button style=\"{button_style}\" onclick=\"window.open('{intent['link']}', '_blank')\">Click me</button>"
+                else:
+                    return random.choice(intent['responses'])
+    
+    return f"To Calculate Your Loan <button style=\"{button_style}\" onclick=\"window.open('{default_link}', '_blank')\">Click here</button>"
+
+
+if __name__ == "__main__":
+    print("Let's chat! (type 'quit' to exit)")
+    while True:
+        # sentence = "do you use credit cards?"
+        sentence = input("You: ")
+        if sentence == "quit":
+            break
+
+        resp = get_response(sentence)
+        print(resp)
+
