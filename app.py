73c79d904f477b81f25bdb2526afc85a255fecd2
Tuba_Khan <121151121+tubakhan0415@users.noreply.github.com>
2024-05-29 16:45:58 +0530

    Update app.py

--- app.py
+++ app.py
@@ -19,4 +19,6 @@ def predict():
     return jsonify(message)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+     port = int(os.environ.get('PORT', 5000))
+     app.run(host='0.0.0.0', port=port)
+   
